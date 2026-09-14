4214090f1b6ea1226a7921b91bfc68dab03ac0cb
ADINATH18 <dalviadinath7777@gmail.com>
2026-09-14 20:38:03 +0530

    Allow Vercel frontend for Socket.IO

--- Server/server.js
+++ Server/server.js
@@ -23,7 +23,10 @@ const httpServer = createServer(app);
 
 const io = new Server(httpServer, {
     cors: {
-        origin: 'http://localhost:5173',
+        origin: [
+            'http://localhost:5173',
+            'https://skill-exchange-fawn-six.vercel.app'
+        ],
         methods: ['GET', 'POST']
     }
 });
